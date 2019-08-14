b79b30bf8524ae22dce33cba0f968f54a729a527
TW-DEV-YangyangCAO <yycao@thoughtworks.com>
2019-08-14 22:51:29 +0800

    feat: add swagger feat

--- pixijs/src/index.js
+++ pixijs/src/index.js
@@ -5,15 +5,19 @@ const app = new PIXI.Application();
 
 document.body.appendChild(app.view);
 
+
 app.loader.add('cat', 'assets/cat.png').load((loader, resources) => {
     const cat = new PIXI.Sprite(resources.cat.texture);
-cat.x = app.renderer.width / 2;
-cat.y = app.renderer.height / 2;
-cat.anchor.x = 0.5;
-cat.anchor.y = 0.5;
-app.stage.addChild(cat);
-app.ticker.add(() => {
-    // each frame we spin the cat around a bit
-    cat.rotation += 0.01;
-});
+    cat.x = app.renderer.width / 2;
+    cat.y = app.renderer.height / 2;
+    cat.anchor.x = 0.5;
+    cat.anchor.y = 0.5;
+    cat.scale.y = 0.5;
+    cat.scale.x = 0.5;
+
+    app.stage.addChild(cat);
+    app.ticker.add(() => {
+        // each frame we spin the cat around a bit
+        cat.rotation += 0.01;
+    });
 });
