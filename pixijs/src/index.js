24f444b761f4e165f9bd4400e4156d9767a79a2c
TW-DEV-YangyangCAO <yycao@thoughtworks.com>
2019-08-14 09:22:42 +0800

    chore: remove unused comment

--- pixijs/src/index.js
+++ pixijs/src/index.js
@@ -1,33 +1,19 @@
 import * as PIXI from 'pixi.js';
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
+
 const app = new PIXI.Application();
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
 document.body.appendChild(app.view);
 
-// load the texture we need
-app.loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
-
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
-
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
-
-// Listen for frame updates
+app.loader.add('cat', 'assets/cat.png').load((loader, resources) => {
+    const cat = new PIXI.Sprite(resources.cat.texture);
+cat.x = app.renderer.width / 2;
+cat.y = app.renderer.height / 2;
+cat.anchor.x = 0.5;
+cat.anchor.y = 0.5;
+app.stage.addChild(cat);
 app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
+    // each frame we spin the cat around a bit
+    cat.rotation += 0.01;
 });
 });
